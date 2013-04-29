1d83819f9ded0f18aafbacb3eac01e42c342a546
Cholathit Khueankaew <kcholathit@gmail.com>
2013-04-29 23:08:21 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogProj::Application.config.secret_key_base = 'cbfa16368f50bc78ae1b5c89f2bdd741c1ca4bb4836c7e691cbe09d67f404c2fc1c33ba4d800c2637493274e82e1abd1c99fead59108ffe3c9cb174104b1d007'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogProj::Application.config.session_store :encrypted_cookie_store, key: '_blog_proj_session'
